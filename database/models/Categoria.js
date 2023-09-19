2120547bf60bbbbc24a50260190d0cb1f1bebd39
joão <joaopedrot1912@gmail.com>
2023-09-19 00:18:48 -0300

    relacionamente muitos para muitos

--- database/models/Categoria.js
+++ database/models/Categoria.js
@@ -1,8 +1,8 @@
 import { DataTypes } from "sequelize"
 import Sequelize from "sequelize"
 import database from "../db.js"
-//import Restaurante from "./Restaurante.js"
 import ListaCategoria from './listaCategoria.js'
+import Restaurante from "./Restaurante.js"
 
 class Categoria extends Sequelize.Model { }
 
@@ -26,11 +26,22 @@ Categoria.init(
     }
 )
 
-// Categoria.belongsToMany(
-//     Restaurante,
-//     {
-//         through: ListaCategoria
-//     }
-// )
+// relacionamento muitos pra muitos
+
+Categoria.belongsToMany(Restaurante, {
+    through: {
+        model: ListaCategoria,
+    },
+    foreignKey: "fk_restaurante",
+    constraints: true
+})
+
+Restaurante.belongsToMany(Categoria, {
+    through: {
+        model: ListaCategoria,
+    },
+    foreignKey: "fk_categoria",
+    constraints: true
+})
 
 export default Categoria
