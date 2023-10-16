aabe5152553a7a319cd6c162d687128d3ddf66a7
Danieli Bordulis <danieli_b_silva@estudante.sesisenai.org.br>
2023-10-16 15:39:56 -0300

    fizemos o banco
    mesa, listaMesas e produtosCarrinho

---
+++ database/models/ListaMesa.js
@@ -0,0 +1,54 @@
+import { DataTypes } from "sequelize"
+import Sequelize from "sequelize"
+import database from "../db.js"
+import Mesa from "./Mesa"
+import Restaurante from "./Restaurante.js"
+
+
+class ListaMesa extends Sequelize.Model { }
+
+ListaMesa.init(
+
+    {
+        id:{
+            primaryKey: true,
+            type: DataTypes.INTEGER,
+            autoIncrement: true
+        }
+    },
+    {
+        sequelize:database,
+        modelName: "ListaMesa",
+        tableName: "Lista_mesas",
+    },   
+)
+
+ListaMesa.belongsTo(
+    Mesa,
+    {
+        constraints: true,
+        foreignKey:'fk_mesa'
+    }
+)
+
+ListaMesa.belongsTo(
+    Restaurante,
+    {
+        constraints: true,
+        foreignKey:'fk_restaurante'
+    }
+)
+
+Restaurante.belongsToMany(
+    Mesa,
+    {
+        through:{
+            model:ListaMesa
+        },
+        constraints: true,
+        foreignKey:'fk_mesa'
+    }
+)
+
+export default ListaMesa
+
