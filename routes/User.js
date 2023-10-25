12b3f75eb79ca64a1ca859b3b1faf289ebbf7948
Dalison <dalisonmg@gmail.com>
2023-10-25 16:58:47 -0300

    Teste

--- routes/User.js
+++ routes/User.js
@@ -1,10 +1,10 @@
 import express from "express"
-const router = express.Router()
 import UserController from "../database/controllers/userController.js"
-const userController = new UserController()
-
 import AuthToken from "../Middlewares/AuthToken.js"
+
 const authToken = new AuthToken()
+const userController = new UserController()
+const router = express.Router()
 
 router.get("/", authToken.execute, userController.getUsers)
 
@@ -12,6 +12,6 @@ router.get("/auth", authToken.execute, userController.auth)
 
 router.get('/getUser', authToken.execute, userController.getUser)
 
-router.post("/newUser", authToken.execute ,userController.createUser)
+router.post("/newUser", authToken.execute, userController.createUser)
 
 export default router
