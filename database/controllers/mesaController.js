e8d969f5cba2f6c10a384966e3843f82c115eed5
Zuma Elma Pereira <zumapereira@hotmail.com>
2023-11-16 16:35:38 -0300

    rotas mesas prontas

--- database/controllers/mesaController.js
+++ database/controllers/mesaController.js
@@ -1,5 +1,5 @@
 import Mesa from "../models/Mesa.js"
-import ListaMesa from"../models/ListaMesa.js"
+import ListaMesa from "../models/ListaMesa.js"
 import { CustomError } from "../../Middlewares/erros.js"
 
 
@@ -7,7 +7,7 @@ class mesaController {
 
     async pegarMesas(req, res) {
 
-        const {idRestaurante} = req.params
+        const { idRestaurante } = req.params
 
         const listaMesas = await ListaMesa.findAll({
             where: {
@@ -18,10 +18,10 @@ class mesaController {
         if (!listaMesas || listaMesas.length == 0) {
             throw new CustomError("Este restaurante não possui mesas.", 404)
         }
-        
-        const mesas=[]
 
-        for(let obj of listaMesas) {
+        const mesas = []
+
+        for (let obj of listaMesas) {
 
             const mesa = await Mesa.findByPk(obj.fk_mesa)
             mesas.push(mesa)
@@ -29,11 +29,103 @@ class mesaController {
         }
 
         res.status(200).json({
-           status: 'success',
-           mesas:mesas 
+            status: 'success',
+            mesas: mesas
         })
 
     }
+
+    async criarMesa(req, res) {
+        const { idRestaurante } = req.params
+        const qrcode = req.file
+
+        let mesa
+
+        try {
+            mesa = await Mesa.create({
+                qr_code: `http://45.224.129.126:8085/files/${qrcode.filename}`,
+                conta: 0
+            })
+
+            await ListaMesa.create({
+                fk_restaurante: idRestaurante,
+                fk_mesa: mesa.id,
+            })
+
+        } catch (error) {
+            throw new CustomError("O servidor falhou em criar a mesa", 500)
+        }
+
+
+        res.status(200).json({
+            status: "success",
+            mesa:mesa
+        })
+    }
+
+    async trocarOcupado(req,res){
+        const {idMesa} = req.params
+        const {isOccupied} = req.body
+
+        const mesa = await Mesa.findByPk(idMesa)
+
+        if(!mesa){
+            throw new CustomError("Esta mesa não foi encontrada", 404)
+        }
+
+        if (mesa.ocupada != isOccupied) {
+
+            mesa.set({
+                ocupada: isOccupied
+            })
+
+            mesa.save()
+
+            res.status(200).json({
+                status:'success',
+                message: "Troca efetuada"
+            })
+
+        }
+
+        let message = isOccupied ? "Já está ocupada." : "Já está livre."
+
+        res.status(200).json({
+            status:'success',
+            message: message
+        })
+
+    }
+
+    async deletarMesa(req,res){
+        const {idMesa} = req.params
+
+        if (!idMesa) {
+            throw new CustomError("Id inválido", 400)
+        }
+
+        try {
+            await ListaMesa.destroy({
+                where: {
+                    fk_mesa: idMesa
+                }
+            })
+
+            await Mesa.destroy({
+                where:{
+                   id:idMesa 
+                }
+            })
+        } catch (error) {
+            throw new CustomError("A mesa não pode ser deletada", 500)
+        }
+
+        res.status(200).json({
+            status:'success',
+            message:'Mesa deletada com sucesso!'
+        })
+
+    }    
 
 }
 
--- routes/Mesa.js
+++ routes/Mesa.js
@@ -1,13 +1,24 @@
 import express from "express";
 import mesaController from "../database/controllers/mesaController.js";
 import AuthToken  from "../Middlewares/AuthToken.js"
+import multer from "multer"
+import { storage } from "../Middlewares/MulterConfig.js"
+
 const authToken = new AuthToken()
+
+const upload = multer({storage})
 
 const MesaController = new mesaController()
 
 const router = express.Router()
 
 router.get("/:idRestaurante", authToken.execute, MesaController.pegarMesas)
+
+router.post("/add/:idRestaurante", authToken.execute, upload.single('qrcode') ,MesaController.criarMesa)
+
+router.put("/toggleOccupied/:idMesa", authToken.execute, MesaController.trocarOcupado)
+
+router.delete("/delete/:idMesa", authToken.execute, MesaController.deletarMesa)
 
 
 export default router
