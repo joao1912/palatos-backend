2d436d69444b886bd64ed894aa9b089bca82255e
joão <joaopedrot1912@gmail.com>
2023-11-21 21:12:46 -0300

    force

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import swaggerUi from "swagger-ui-express"
 import "./database/assossiations.js"
 
 ;(async function(){
-    await database.sync()
+    await database.sync({force: true})
 })()
 
 import restaurantRoutes from "./routes/Restaurante.js"
