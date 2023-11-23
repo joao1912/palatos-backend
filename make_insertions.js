3c6de70000a57412fb65f67e6ccfd101afa040d3
Dalison <dalisonmg@gmail.com>
2023-11-23 16:46:14 -0300

    aff

--- make_insertions.js
+++ make_insertions.js
@@ -13,5 +13,5 @@ import create_categorias from "./utils/create_categorias.js"
 import create_comandas from "./utils/create_comandas.js"
 import setReservas from "./utils/create_reservas.js"
 
-//create_comandas()
-setReservas()
+create_comandas()
+//setReservas()
